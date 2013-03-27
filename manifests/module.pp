27dbb565d3299d223616c8c16f81e8fdf6d9a225
Simon Piette <piette.simon@gmail.com>
2013-03-27 15:13:44 -0400

    modules.pp: puppet-linted to 80 chars

--- manifests/module.pp
+++ manifests/module.pp
@@ -1,18 +1,21 @@
 # Definition: selinux::module
 #
 # Description
-#  This class will either install or uninstall a SELinux module from a running system.
-#  This module allows an admin to keep .te files in text form in a repository, while
-#  allowing the system to compile and manage SELinux modules.
+#  This class will either install or uninstall a SELinux module from a running
+#  system.
+#  This module allows an admin to keep .te files in text form in a repository,
+#  while allowing the system to compile and manage SELinux modules.
 #
 #  Concepts incorporated from:
 #  http://stuckinadoloop.wordpress.com/2011/06/15/puppet-managed-deployment-of-selinux-modules/
 #
 # Parameters:
 #   - $ensure: (present|absent) - sets the state for a module
-#   - $selinux::params::sx_mod_dir: The directory compiled modules will live on a system (default: /usr/share/selinux)
+#   - $selinux::params::modules_dir: The directory compiled modules will live on
+#   a system (default: /usr/share/selinux)
 #   - $mode: Allows an admin to set the SELinux status. (default: enforcing)
-#   - $source: the source file (either a puppet URI or local file) of the SELinux .te module
+#   - $source: the source file (either a puppet URI or local file) of the
+#   SELinux .te module
 #
 # Actions:
 #  Compiles a module using 'checkmodule' and 'semodule_package'.
@@ -39,21 +42,22 @@ define selinux::module(
 
   # Only allow refresh in the event that the initial .te file is updated.
   Exec {
-    path         => '/sbin:/usr/sbin:/bin:/usr/bin',
-    refreshonly  => true,
-    cwd          => $selinux::params::sx_mod_dir,
+    path        => '/sbin:/usr/sbin:/bin:/usr/bin',
+    refreshonly => true,
+    cwd         => $selinux::params::modules_dir,
   }
 
   ## Begin Configuration
-  file { "${selinux::params::sx_mod_dir}/${name}.te":
-    ensure => $ensure,
-    source => $source,
-    tag    => 'selinux-module',
+  file { "${selinux::params::modules_dir}/${name}.te":
+    ensure  => $ensure,
+    source  => $source,
+    tag     => 'selinux-module',
+    require => File[$selinux::params::modules_dir],
   }
-  file { "${selinux::params::sx_mod_dir}/${name}.mod":
+  file { "${selinux::params::modules_dir}/${name}.mod":
     tag => ['selinux-module-build', 'selinux-module'],
   }
-  file { "${selinux::params::sx_mod_dir}/${name}.pp":
+  file { "${selinux::params::modules_dir}/${name}.pp":
     tag => ['selinux-module-build', 'selinux-module'],
   }
 
@@ -71,7 +75,7 @@ define selinux::module(
       }
 
       # Set dependency ordering
-      File["${selinux::params::sx_mod_dir}/${name}.te"]
+      File["${selinux::params::modules_dir}/${name}.te"]
       ~> Exec["${name}-buildmod"]
       ~> Exec["${name}-buildpp"]
       ~> Exec["${name}-install"]
