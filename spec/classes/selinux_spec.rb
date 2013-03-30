b0eb9fb4b1af2c6256bb1dc28921598e742ac213
Simon Piette <piette.simon@gmail.com>
2013-03-29 20:30:30 -0400

    add test so no setenforce are run when disabled is in

--- spec/classes/selinux_spec.rb
+++ spec/classes/selinux_spec.rb
@@ -15,15 +15,17 @@ describe 'selinux' do
 
         it { should create_class('selinux') }
         it { should create_class('stdlib') }
+        it { should create_class('selinux::params') }
         it { should create_class('selinux::config') }
         it { should create_file('/etc/sysconfig/selinux')\
           .with_content(/^SELINUX=#{param_mode}$/) }
         if current_mode != param_mode
           # we have to exec setenforce
           if  current_mode != 'disabled'  and  param_mode != 'disabled' 
-            it { should create_exec("setenforce-#{param_mode}") }
             it { should create_exec("setenforce-#{param_mode}")\
             .with_command("setenforce #{param_mode}") }
+          else
+            it { should_not create_exec("setenforce-#{param_mode}") }
           end
         end
       end
