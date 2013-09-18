462e49f0e32c6b1e4df86d15a20d3c328a0e51c5
Jan Vansteenkiste <jan@vstone.eu>
2013-09-18 13:18:34 +0200

    Fix mixup between enforcing and enforced but allow either one to be used by end-user

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,19 +4,19 @@
 #
 # === Parameters:
 #  [*mode*]
-#   (enforced|permissive|disabled) - sets the operating state for SELinux.
+#   (enforcing|permissive|disabled) - sets the operating state for SELinux.
 #
-# Actions:
-#  Configures SELinux to a specific state (enforced|permissive|disabled)
+# === Actions:
+#  Configures SELinux to a specific state (enforcing|permissive|disabled)
 #
-# Requires:
+# === Requires:
 #  This module has no requirements
 #
-# Sample Usage:
+# === Sample Usage:
 #  This module should not be called directly.
 #
 class selinux::config(
-  $mode=undef
+  $mode
 ) {
   Exec {
     path => '/bin:/sbin:/usr/bin:/usr/sbin',
@@ -36,15 +36,22 @@ class selinux::config(
 
   $current_mode = $::selinux? {
     'false' => 'disabled',
-    default => $::selinux_current_mode
+    false   => 'disabled',
+    default => $::selinux_current_mode,
   }
+
+  $real_mode = $mode ? {
+    /^enforc/ => 'enforcing',
+    default   => $mode,
+  }
+
   # we don't always run setenforce
-  if $current_mode != $mode {
+  if $current_mode != $real_mode {
     # only if there's change
-    case $mode {
+    case $real_mode {
       'disabled': {
         # we can't apply right now disabled, but we can set it to permissive
-        $change = "from ${current_mode} to ${mode}"
+        $change = "from ${current_mode} to ${real_mode}"
         notify { 'change':
           message => "A reboot is required to change ${change}"
         }
@@ -57,19 +64,19 @@ class selinux::config(
       /^(permissive|enforcing)$/: {
         if $current_mode == 'disabled' {
           # we can't set disabled now, it needs a reboot.
-          $change = "from ${current_mode} to ${mode}"
+          $change = "from ${current_mode} to ${real_mode}"
           notify { 'change':
             message => "A reboot is required to change ${change}"
           }
         } else {
           # we're going from permissive to enforcing or vice-versa
-          exec { "setenforce ${mode}":
+          exec { "setenforce ${real_mode}":
             require => Package['libselinux-utils'],
           }
         }
       }
       default: {
-        fail('You must specify a mode (enforced, permissive, or disabled)')
+        fail('You must specify a mode (enforcing, permissive, or disabled)')
       }
     }
   }
